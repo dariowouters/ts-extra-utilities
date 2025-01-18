41b307496960732571de8e43100adba75cd115a1
Dario Wouters <dariowouters@gmail.com>
2025-01-18 10:24:27 +0100

    bump to 1.0.1
    Fixed DirectX hook not working on some devices

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+1.0.1
+    - Fixed DirectX hook not working on some devices
+
 1.0.0
     - Initial Version
--- src/version.hpp
+++ src/version.hpp
@@ -2,5 +2,5 @@
 
 namespace ts_extra_utilities
 {
-    constexpr const char* VERSION = "1.0.0";
+    constexpr const char* VERSION = "1.0.1";
 }
